f43ed6c646eb225af5e50c49e6b3baa97c263bac
Md. Masum Musfique <66321598+Masumiub@users.noreply.github.com>
2023-07-07 17:00:44 +0600

    init

---
+++ contracts/First/src/FirstContract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Numerics;
+
+using Neo;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Attributes;
+using Neo.SmartContract.Framework.Native;
+using Neo.SmartContract.Framework.Services;
+
+namespace First{
+    public class FirstContract : SmartContract{
+        public static void AddData(object data){
+            Storage.Put(Storage.CurrentContext, "Name", "Masum");
+        }
+
+        public static string getData(){
+            return Storage.Get(Storage.CurrentContext, "Name");
+        }
+    }
+}
